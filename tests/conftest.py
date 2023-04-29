09b231bdd34bb52a116d82d243cad1e3b9da4fae
ReachPeach <maxim.busyuk@gmail.com>
2023-04-30 01:01:43 +0300

    Create tests for fronted MVP functionality

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,16 @@
+import threading
+
 import pytest
 
+from app import index
 from app import app as App
+from selenium import webdriver
 
 
 @pytest.fixture
 def app():
+    tread = threading.Thread(target=App.run)
+    tread.start()
     return App
 
 
@@ -16,3 +22,11 @@ def client(app):
 @pytest.fixture
 def runner(app):
     return app.test_cli_runner()
+
+
+@pytest.fixture()
+def driver(app):
+    driver = webdriver.Chrome()
+    driver.get('http://127.0.0.1:5000/api/v1/file/index.html')
+    yield driver
+    driver.quit()
