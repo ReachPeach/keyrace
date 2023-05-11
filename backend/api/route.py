069fd51471bf463f11aee4fc308a7ac99c06e191
EasySocHacks <easy.soc.hacks@gmail.com>
2023-05-12 00:05:38 +0300

    Add logging

--- backend/api/route.py
+++ backend/api/route.py
@@ -4,12 +4,19 @@ from typing import Callable
 
 import flask
 
+from log import get_logger
+from backend.utils import generate_id
+
+LOG = get_logger()
+
 
 def base_rote(
         blueprint: flask.Blueprint,
         method: str,
         url: str,
 ):
+    LOG.debug(f"Register route '{blueprint.url_prefix}{url}' with method '{method}'")
+
     def decorator(function: Callable):
         @functools.wraps(function)
         @blueprint.route(
@@ -17,9 +24,19 @@ def base_rote(
             methods=[method],
         )
         def wrapper(*args, **kwargs):
+            request_id = generate_id()
+
             try:
-                return function(*args, **kwargs)
+                LOG.info(f"{method} call: '{blueprint.url_prefix}{url}'. Request id = {request_id}")
+
+                ret = function(*args, **kwargs)
+
+                LOG.info(f"Call done. Request id = {request_id}")
+
+                return ret
             except Exception as e:
+                LOG.error(f"Call fails with error '{str(e)}'. Request id = {request_id}")
+
                 return str(e), http.HTTPStatus.INTERNAL_SERVER_ERROR
 
         return wrapper
