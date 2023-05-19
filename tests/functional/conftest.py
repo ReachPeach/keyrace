a09e0b0570cb8cb0ff509bbf2e55a29a1e72934f
EasySocHacks <easy.soc.hacks@gmail.com>
2023-05-19 23:43:28 +0300

    Fix backend tests

---
+++ tests/functional/conftest.py
@@ -0,0 +1,31 @@
+import threading
+
+import pytest
+from selenium import webdriver
+
+from app import app as App
+
+
+@pytest.fixture
+def app():
+    tread = threading.Thread(target=App.run)
+    tread.start()
+    return App
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
+
+
+@pytest.fixture()
+def driver(app):
+    driver = webdriver.Chrome()
+    driver.get('http://127.0.0.1:5000/api/v1/file/index.html')
+    yield driver
+    driver.quit()
